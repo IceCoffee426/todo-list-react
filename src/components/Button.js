211c37c78ae729b39aa3ac5f708e7533f7e54e46
Shoaib Mastan <114753586+IceCoffee426@users.noreply.github.com>
2023-04-08 12:42:02 +0100

    Add count state for button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,15 +3,21 @@ import React, { Component } from "react";
 export default class Button extends Component {
   constructor(props) {
     super(props);
-    this.sayHi = this.sayHi.bind(this);
+    this.state = {
+      count: 0,
+    };
+    // this.sayHi = this.sayHi.bind(this);
   }
 
-  sayHi() {
-    alert("Hello world! Welcome to my React app.");
-    console.log(`You clicked on ${this.constructor.name}`);
-  }
+  // sayHi() {
+  //   alert("Hello world! Welcome to my React app.");
+  //   this.state.count += 1;
+  //   console.log(
+  //     `You clicked on ${this.constructor.name} ${this.state.count} times`
+  //   );
+  // }
 
   render() {
-    return <button onClick={this.sayHi}>Hello</button>;
+    return <button onClick={this.props.count}>Hello</button>;
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,27 @@ import Button from "./Button";
 export default class Header extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0,
+    };
+    this.countUp = this.countUp.bind(this);
+  }
+
+  countUp() {
+    // MUST ALWAYS USE SET STATE, DO NOT MODIFY STATE WITHOUT USING THIS FUNCTION
+    this.setState({ count: (this.state.count += 1) });
+    alert("Hello world! Welcome to my React app.");
+    console.log(`${this.state.count} clicks`);
   }
 
   render() {
     return (
       <div className="header">
         <h1 className="title">{this.props.title}</h1>
-        <Button />
+        <Button count={this.countUp} />
+        <p className="count">
+          You clicked on the button {this.state.count} time(s).
+        </p>
       </div>
     );
   }
