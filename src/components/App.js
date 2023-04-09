ede2e75d406bc5da67cd7a0147d7f6f7ffa3990f
Shoaib Mastan <114753586+IceCoffee426@users.noreply.github.com>
2023-04-09 01:01:02 +0100

    Allow user to add to-do's using form

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,46 @@
 import React, { Component, useState } from "react";
-import Header from "./Header";
-import Form from "./Form";
 import Overview from "./Overview";
 
 export default function App() {
   const [task, setTask] = useState({ text: "" });
   const [tasks, setTasks] = useState([]);
+
+  const handleChange = (e) => {
+    setTask({ text: e.target.value });
+  };
+
+  const submitTask = (e) => {
+    e.preventDefault();
+    // Not using "push" here as state should be treated as immutable!!
+    setTasks(tasks.concat(task));
+    setTask({ text: "" });
+  };
+
   return (
     <div className="app">
-      <Header appName="React Notes App" />
-      <Form />
-      <Overview />
+      <header className="header">
+        <img src="./favicon.svg" className="logo"></img>
+        <h1 className="title">To-Do List</h1>
+      </header>
+      <main>
+        <form className="input-form">
+          <input
+            type="text"
+            placeholder="Enter task"
+            id="taskInput"
+            value={task.text}
+            onChange={handleChange}
+          />
+          <button type="submit" onClick={submitTask}>
+            Add Task
+          </button>
+        </form>
+        <Overview tasks={tasks} />
+      </main>
+      <footer>
+        <a href="https://github.com/IceCoffee426">IceCoffee426</a>
+        <p>&copy; MMXXIII</p>
+      </footer>
       {/* Other components in the app will go here */}
     </div>
   );
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function Overview() {
-  return <div className="overview">Notes will go here.</div>;
+export default function Overview({ tasks }) {
+  return (
+    <ul className="task-list">
+      {tasks.map((task) => {
+        return <li>{task.text}</li>;
+      })}
+    </ul>
+  );
 }
